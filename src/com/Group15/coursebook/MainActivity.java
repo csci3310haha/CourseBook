473613e2545696a4acf91ac46d3f0b1daa58a472
FUN <FUN@192.168.216.71>
2012-12-22 18:59:33 +0800

    try

--- src/com/Group15/coursebook/MainActivity.java
+++ src/com/Group15/coursebook/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+//Edward Change
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
