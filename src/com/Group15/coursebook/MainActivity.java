791e4918c7403e63b25d8eb1ebbccc88fe01ef7c
Digi-time <Digi-time@Digi-time-PC>
2012-12-24 14:56:08 -0800

    Add new activity index

--- src/com/Group15/coursebook/MainActivity.java
+++ src/com/Group15/coursebook/MainActivity.java
@@ -1,29 +1,28 @@
 package com.Group15.coursebook;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
-//Edward Change
-	//Andy Change
-	//edward change second
-	//edward change third
-	//edward change forth
-	//edward change fifth
-	//Andy Change second
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		int i=0;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void toAnotherActivity(View view){
+		Intent intent = new Intent(this, Index.class);
+		startActivity(intent);
 	}
 
 }
